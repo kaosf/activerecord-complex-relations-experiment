49483a262e531eabd031dcd6bb41e84d3494d3c6
ka <ka.kaosf@gmail.com>
2015-01-26 03:39:39 +0900

    Implement complex relationships
    ref. http://stackoverflow.com/questions/6762919/ruby-on-rails-has-many-through-self-referential-following-follower-relationships

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,3 +1,4 @@
 class Friend < ActiveRecord::Base
   belongs_to :user
+  belongs_to :to_user, foreign_key: :to_user_id, class: User
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  has_many :friends, dependent: :destroy
+  has_many :friends_from, foreign_key: :to_user_id, class: Friend
+  has_many :books, dependent: :destroy
+
+  has_many :friend_users, through: :friends, class: User, source: :to_user
+  has_many :friend_user_books, through: :friend_users, class: Book, source: :books
 end
